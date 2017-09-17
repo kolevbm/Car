a648fd2734e2f999c7b2022b5fc1e433151d5533
kolevbm <kolev.bm@gmail.com>
2017-09-18 02:54:27 +0300

    Update servo.ino

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,4 +1,6 @@
-#include <Servo.h>
+#include <Servo.h> //when using servo library On boards other than the Mega,
+//use of the library disables analogWrite() (PWM) functionality on pins 9 and 10,
+//whether or not there is a Servo on those pins
 
 
             //defines my servo
